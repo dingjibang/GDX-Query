d0ff5294b137827a0e0302a190a2afc4cd9064ed
dingjibang <dingjibang@qq.com>
2015-06-24 14:56:06 +0800

    add 'onClick' method

--- src/demo/SimpleGdxQueryDemo.java
+++ src/demo/SimpleGdxQueryDemo.java
@@ -2,13 +2,15 @@ package demo;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.rpsg.gdxQuery.$;
+import com.rpsg.gdxQuery.GdxQuery;
 
-public class SimpleGdxQueryDemo implements ApplicationListener {
+public class SimpleGdxQueryDemo implements ApplicationListener, InputProcessor {
 	public static void main(String[] args) {
 		new LwjglApplication(new SimpleGdxQueryDemo(),"demo",1024,576);
 	}
@@ -27,33 +29,48 @@ public class SimpleGdxQueryDemo implements ApplicationListener {
 		
 		//现在我们用GdxQuery模式，创建一个GdxQuery对象，并使用它。
 		//now ,use GdxQuery to create a Actor and add to stage
-		stage.addActor($.add(new Image(new Texture(Gdx.files.internal("resource/data/badlogic.jpg")))).setPosition(150, 160)
-			.setColor(1,0.3f,1f,0.5f).getItem());
+		stage.addActor($.add(new Image(new Texture(Gdx.files.internal("resource/data/badlogic.jpg")))).setPosition(150, 160).setColor(1,0.3f,1f,0.5f).getItem());
 		
 		//还可以简化!
 		//more simplified!
-		$.image("resource/data/badlogic.jpg").setPosition(300, 300).appendTo(stage);
+		GdxQuery q=$.image("resource/data/badlogic.jpg").setPosition(300, 300).appendTo(stage);
 		
+		//所有的query对象你都可以调用一个叫onClick的方法，传入一个runnable，当这个对象被单击的时候，将会执行runnable。
+		//any Query Object allow to use "onClick" method to give a click callback.
+		q.onClick(new Runnable() {
+			public void run() {
+				System.out.println("image was clicked!");
+			}
+		});
 	}
 	
 	public void render() {
 		stage.act();
 		stage.draw();
+		Gdx.input.setInputProcessor(this);
 	}
 
-	public void dispose() {
-		
-	}
+	public void dispose() { }
 	
-	public void pause() {
-		
-	}
+	public void pause() { }
 	
-	public void resize(int arg0, int arg1) {
-		
-	}
+	public void resize(int arg0, int arg1) { }
 
-	public void resume() {
-		
-	}
+	public void resume() { }
+
+	public boolean keyDown(int arg0) { return stage.keyDown(arg0); }
+
+	public boolean keyTyped(char arg0) { return stage.keyTyped(arg0); }
+
+	public boolean keyUp(int arg0) { return stage.keyUp(arg0); }
+
+	public boolean mouseMoved(int arg0, int arg1) { return stage.mouseMoved(arg0, arg1); }
+
+	public boolean scrolled(int arg0) { return stage.scrolled(arg0); }
+
+	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) { return stage.touchDown(arg0, arg1, arg2, arg3); }
+
+	public boolean touchDragged(int arg0, int arg1, int arg2) { return stage.touchDragged(arg0, arg1, arg2); }
+
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) { return stage.touchUp(arg0, arg1, arg2, arg3); }
 }
